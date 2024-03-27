4577410fb2c518f7b20272dcf1bfb9f63281c2a9
mcdo110492 <fuentebellamcdonald@gmail.com>
2024-03-27 21:25:48 +0800

    Initial Commit

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>One Date Selection</h1>
+    <VueDatePicker v-model="date"></VueDatePicker>
+    <h1>Minimum Date Selection</h1>
+    <VueDatePicker v-model="minDate" :min-date="new Date()"></VueDatePicker>
+    <h1>Maximum Date Selection</h1>
+    <VueDatePicker v-model="maxDate" :max-date="maxDateLimit"></VueDatePicker>
+    <h1>Date Range Selection</h1>
+    <VueDatePicker v-model="rangeDate" range />
+</template>
+
+<script setup>
+import { computed, ref, onMounted } from "vue"
+import VueDatePicker from "@vuepic/vue-datepicker"
+import "@vuepic/vue-datepicker/dist/main.css"
+import { addMonths, getMonth, getYear } from 'date-fns'
+
+const date = ref()
+const minDate = ref()
+const maxDate = ref()
+const rangeDate = ref()
+
+const maxDateLimit = computed(() => addMonths(new Date(getYear(new Date()), getMonth(new Date())), 2))
+
+onMounted(() => {
+    const startDate = new Date()
+    const endDate = new Date(new Date().setDate(startDate.getDate() + 7))
+    rangeDate.value = [startDate, endDate]
+})
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import DatePicker from '../components/DatePicker.vue';
+</script>
+
+<template>
+  <main>
+    <DatePicker />
+  </main>
+</template>
